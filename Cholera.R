8929b88a799608b2cde630392dec6e288273bf66
marialma <i.am.maria.ma@gmail.com>
2017-08-28 16:00:19 -0700

    Add files via upload
    Finished, I think?

--- Cholera.R
+++ Cholera.R
@@ -3,12 +3,14 @@ library(dplyr)
 setwd("~/Documents/Yemen Cholera")
 choleradata <- read.csv("Yemen Cholera Outbreak Epidemiology Data - Data_Governorate_Level.csv")
 
-# Dropping unnecessary variables. There's probably a more elegant way to do this.
+# Dropping unnecessary variables. There's probably a less dumb way to do this.
+
 cholera <- mutate(choleradata,COD.Gov.Arabic = NULL) %>%
   mutate(COD.Gov.Pcode = NULL) %>%
   mutate(Governorate = NULL)
 
 # Renaming variables to be easier to type
+
 cholera <-  rename(cholera, 
          attack_rate = "Attack.Rate..per.1000.",
          case_fatality = "CFR....",
@@ -24,6 +26,7 @@ cholera$Date <- cholera$Date %>% as.character %>% as.Date
 # Data downloaded as of 8/28 is inconsistent - attack rates are off by a factor of 10. This isn't real;
 # I checked against the original data to confirm this. So, gotta fix that. 
 # Divide attack rate by 10 for entries after Aug 14th. Is there a way to detect this automatically?
+
 cholera <- mutate(cholera, attack_rate = ifelse(cholera$Date > "2017-08-14", attack_rate/10, attack_rate))
 
 # Hadramaut is split into Say'on and Moklla, but data for this isn't available until after July. 
@@ -33,7 +36,6 @@ hadramaut <- cholera %>%
   filter(COD == "#N/A") %>%
   group_by(Date) %>%
   summarise(Cases = sum(Cases), Deaths = sum(Deaths), case_fatality = mean(case_fatality), attack_rate = mean(attack_rate), COD = "Hadramaut")
-
 
 # summarise won't let me recalculate CFRs, since "deaths/cases" is not a summary statistic, I guess?
 # so now, I'm going to replace them. Rounding to make it consistent with the rest of the data 
@@ -47,11 +49,13 @@ cholera$COD <- as.character(cholera$COD)
 cholera <- cholera %>%
   subset(COD != "#N/A") %>%
   bind_rows(hadramaut)
-
 cholera$COD <- as.factor(cholera$COD)
 
-# Figure out new daily cases
+# Figure out new daily cases. This part was unashamedly taken from someone on stackexchange.
 cholera <- cholera %>%
   group_by(COD) %>%
   arrange(Date) %>%
-  mutate(newcases = Cases - lag(Cases, default=first(Cases)))
+  mutate(newcases = Cases - lag(Cases, default=0))
+
+#write to CSV
+write.csv(cholera, file = "2017YemenCholera.csv")
